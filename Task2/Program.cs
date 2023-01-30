a6b94b43b5eb88666bbb2e1f28eb2960a3d25abd
Evgeny <evch1184@inbox.ru>
2023-01-30 10:10:20 +0300

    done task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+   Console.WriteLine(a);
+else if (a < b)
+   Console.WriteLine(b);
+else    
+Console.WriteLine("=");
