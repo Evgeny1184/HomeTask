ea22729a9ecb8d00afe488c688087a48f9dc0a38
Evgeny <evch1184@inbox.ru>
2023-01-31 14:19:52 +0300

    Task 2.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,8 +5,8 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 if (a > b)
-   Console.WriteLine(a);
-else if (a < b)
-   Console.WriteLine(b);
+ Console.WriteLine($"max = {a}");
+if (a < b)
+ Console.WriteLine($"max = {b}");
 else    
 Console.WriteLine("=");
