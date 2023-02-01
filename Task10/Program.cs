128cac379ca38df53f12cdadf6af4a24bc591dde
Evgeny <evch1184@inbox.ru>
2023-02-01 13:10:04 +0300

    done Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int index = n / 10 % 10;
+while(n < 100 || n >= 1000)
+  {
+     Console.Write("Введите трехзначное число: ");
+     n = Convert.ToInt32(Console.ReadLine());
+  
+  }
+
+Console.WriteLine(index);
+
+
+    
+
+
+
+
+
