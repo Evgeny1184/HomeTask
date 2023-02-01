637161b292f0bd25014d7faca4a48a657043f4d9
Evgeny <evch1184@inbox.ru>
2023-02-01 10:26:33 +0300

    done Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = new Random().Next(1, n);
+int result = 1;
+
+while(result <= n)
+{
+  if(result % 2 == 0)
+  {
+    Console.WriteLine(result);
+
+  }result++;
+}
+
