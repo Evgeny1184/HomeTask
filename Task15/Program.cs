b8330ac92759e5c1dfb02d825ab2e69422da0a59
Evgeny <evch1184@inbox.ru>
2023-02-04 16:18:24 +0300

    done Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите цифру: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n == 6 || n == 7)
+{
+ Console.WriteLine("yes");
+}
+else if(n >= 1)
+{
+  Console.WriteLine("No");
+}
