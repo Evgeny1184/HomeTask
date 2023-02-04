8c1ae3ec52e0a6bb3b4715bd8d04a896d43788e1
Evgeny <evch1184@inbox.ru>
2023-02-04 15:40:16 +0300

    done Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = -1;
+
+while(n < 100)
+{
+  Console.WriteLine("Третьей цифры нет!");
+  Console.Write("Введите число еще раз: ");
+  n = Convert.ToInt32(Console.ReadLine());
+}
+
+if(n > 999)
+{
+  n = n / 10;
+}
+result = n % 10;
+
+if(n > 9999)
+{
+  n = n / 100;
+  
+} 
+result = n % 10;
+Console.WriteLine(result);  
+
