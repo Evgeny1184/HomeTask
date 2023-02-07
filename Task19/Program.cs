569a19c755be01c8158bf1d2f96de5071b3c5666
Evgeny <evch1184@inbox.ru>
2023-02-07 17:31:00 +0300

    done Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int num = number.Length;
+
+if (num == 5)
+{
+   if (number[0] == number[4] && number[1] == number[3])
+   {
+      Console.WriteLine("ДА");
+   }
+   else
+   {
+      Console.WriteLine("НЕТ");
+   }
+
+}
+else
+ {
+   Console.WriteLine("Ошибка! Ввведите пятизначное число! ");
+ }
+
