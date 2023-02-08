79dc5a9374b216b6822d64c2ef95909be81251d0
Evgeny <evch1184@inbox.ru>
2023-02-08 17:05:06 +0300

    done Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+ToDegree(a, b);
+
+void ToDegree(int a, int b)
+{
+  int result = 1;  
+  for (int i = 1; i <= b; i++)
+  {
+    result = result * a;
+  }
+  Console.WriteLine(result);
+}
+  
