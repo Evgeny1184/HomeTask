173128cebff2b1363888aa4ca7bcbb2c497f8eab
Evgeny <evch1184@inbox.ru>
2023-02-08 18:06:39 +0300

    done Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int length = n;
+SumNumbers(n, length);
+
+void SumNumbers(int n, int length)
+{
+  int sum = 0;
+  for (int i = 1; i <= length; i++)
+    {
+      sum += n % 10;
+      n /= 10;
+    }
+    Console.WriteLine(sum);
+}
