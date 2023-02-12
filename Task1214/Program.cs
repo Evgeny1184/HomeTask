23049fdca40a53a8027d8cbe1f79178fa0ce1285
Evgeny <evch1184@inbox.ru>
2023-02-12 18:09:45 +0300

    done Task1214

---
+++ Task1214/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+   array[i] = new Random().Next(1, 10);
+}   
+Console.WriteLine($"{string.Join(" ", array)}");
+
+Console.Write("Введите число массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+if(array[i] == num)
+{
+   count++;
+}
+Console.WriteLine(count);
+  
