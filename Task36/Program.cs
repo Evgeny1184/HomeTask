e141d6fce2da20c2deaa9fbd0b256f8ada5d8557
Evgeny <evch1184@inbox.ru>
2023-02-14 17:26:53 +0300

    done Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-99, 100);
+}    
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+if(i % 2 != 0)
+{
+   sum = sum + array[i]; 
+}
+Console.WriteLine(sum);
