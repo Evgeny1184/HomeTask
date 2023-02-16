a6a00c2eacedc0ef0031531054164e2f277b27e7
Evgeny <evch1184@inbox.ru>
2023-02-16 10:07:37 +0300

    done Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во чисел: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+for (int i = 0; i < n; i++)
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine(count);
+
