9cd05503b345750cbddeddd484a4a331fee568e0
Evgeny <evch1184@inbox.ru>
2023-03-07 21:33:44 +0300

    done Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+void rec(int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    rec(m, n, sum);
+}
+rec(m, n, 0);
+
