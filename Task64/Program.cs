31776c52fc4203a901e59899278782804cceea7a
Evgeny <evch1184@inbox.ru>
2023-03-07 13:48:17 +0300

    done Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+void Revers (int n)
+{
+    if (n == 0) return;
+    Console.Write($"{n} ");
+    Revers (n - 1);
+}
+
+Revers(n);
