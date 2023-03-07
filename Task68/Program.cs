a2b6774d79887dfbd12d01b78117649de9e70234
Evgeny <evch1184@inbox.ru>
2023-03-07 21:45:03 +0300

    done Task68

---
+++ Task68/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Function (int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return Function(m - 1, 1);
+    if (m > 0 && n > 0) return Function(m - 1, Function(m, n - 1));
+return Function(m, n);
+}
+
+Console.Write($"Функция Аккермана для чисел: ({m},{n}) = \n{Function(m, n)}");
